077b38a738b7901a4396d0d9a40f138eec551e76
KamilStrozyk <strozykkamil@gmail.com>
2020-01-04 23:59:26 +0100

    add sample actions

--- Client/src/sample/Controller.java
+++ Client/src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label debugLabel;
+
+    @FXML protected void startGame(ActionEvent event) {
+        debugLabel.setText("Sign in button pressed");
+    }
 }
