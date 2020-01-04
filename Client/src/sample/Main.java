d542dd93fde99678c40fe41140d6fc2eecb36e80
KamilStrozyk <strozykkamil@gmail.com>
2020-01-04 21:07:27 +0100

    Create sample gui scheme

--- Client/src/sample/Main.java
+++ Client/src/sample/Main.java
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        primaryStage.setTitle("Battleship Client");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
