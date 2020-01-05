86f521dcea77a6d2e11413d0e3d3f0ae3766964c
KamilStrozyk <strozykkamil@gmail.com>
2020-01-05 03:21:01 +0100

    everything except grids works

--- Client/src/sample/Controller.java
+++ Client/src/sample/Controller.java
@@ -4,17 +4,16 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 public class Controller {
     final int BUF_SIZE = 512;
+    boolean gameStarted = false;
 
     @FXML
     private Label debugLabel;
@@ -26,6 +25,8 @@ public class Controller {
     private TextField serverPortTextField;
     @FXML
     private TextField playerNameTextField;
+    @FXML
+    private Button startGameButton;
 
     // event listeners
     @FXML
@@ -37,15 +38,27 @@ public class Controller {
             ServerListener serverListener = new ServerListener(clientSocket);
             Thread thread = new Thread(serverListener);
             thread.start();
+            setStartControlsDisabled();
+
         } catch (Exception e) {
             connectionFailed();
         }
 
     }
 
+    private void setStartControlsDisabled() {
+        serverIpTextField.setDisable(true);
+        serverPortTextField.setDisable(true);
+        playerNameTextField.setDisable(true);
+        startGameButton.setDisable(true);
+    }
+
     // resetting app after error or end of game
     private void reset() {
-
+        serverIpTextField.setDisable(false);
+        serverPortTextField.setDisable(false);
+        playerNameTextField.setDisable(false);
+        startGameButton.setDisable(false);
     }
 
     // popups with info, their names are intuitive
@@ -73,6 +86,59 @@ public class Controller {
 
         @Override
         public void run() {
+            waitForEnemy();
+            waitForStart();
+            gameLoop();
+        }
+
+        // waiting for enemy
+        private void waitForEnemy() {
+            boolean enemyChosen = false;
+            String clientMessage = "Player:" + playerNameTextField.getText();
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter(socket.getOutputStream(), true);
+                writer.println(clientMessage);
+            } catch (IOException e) {
+                writingError();
+            }
+            setWaitingMessage(true);
+            while (!enemyChosen) {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverMessage = reader.readLine();
+                    if (serverMessage.contains("enemy")) {
+                        sendEnemyInfo(serverMessage);
+                        enemyChosen = true;
+                        setWaitingMessage(false);
+                    }
+                } catch (Exception e) {
+                    readingError();
+                }
+            }
+        }
+
+        // waiting for start
+        private void waitForStart() {
+            setWaitingMessage(true);
+            while (true) {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverMessage = reader.readLine();
+                    if (serverMessage.contains("start")) {
+                        gameInfo("Game started!");
+                        setWaitingMessage(false);
+                        return;
+                    }
+                } catch (Exception e) {
+                    readingError();
+                }
+            }
+        }
+
+        // main game loop
+        private void gameLoop() {
+
             while (true) {
                 try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -80,13 +146,6 @@ public class Controller {
                     handleServerMessage(serverMessage);
                 } catch (Exception e) {
                     readingError();
-                    try {
-                        socket.close();
-                        reset();
-                        Thread.currentThread().interrupt();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
                 }
             }
         }
@@ -95,9 +154,7 @@ public class Controller {
         private void handleServerMessage(String serverMessage) {
             Platform.runLater(() -> debugLabel.setText(serverMessage));
 
-            if (serverMessage.contains("enemy")) sendEnemyInfo(serverMessage);
-            else if (serverMessage.contains("start")) gameInfo("The game is starting.");
-            else if (serverMessage.contains("Ohit")) handleOurHit(serverMessage);
+            if (serverMessage.contains("Ohit")) handleOurHit(serverMessage);
             else if (serverMessage.contains("Osink")) handleOurHit(serverMessage);
             else if (serverMessage.contains("Omiss")) handleOurMiss(serverMessage);
             else if (serverMessage.contains("hit")) handleHit(serverMessage);
@@ -106,6 +163,7 @@ public class Controller {
             else if (serverMessage.contains("won")) gameInfo("Congratulations! you've won!");
             else if (serverMessage.contains("lose")) gameInfo("You've lost!");
             else if (serverMessage.contains("error")) serverError();
+            else if (serverMessage.contains("turn")) setTurn(serverMessage);
             else {
                 // TODO: Handler for unexpected situations
             }
@@ -116,29 +174,35 @@ public class Controller {
             String enemyName = message.substring(6, message.length());
             Platform.runLater(() -> enemyLabel.setText("Enemy: " + enemyName));
             gameInfo("You're playing with: " + enemyName);
+            setWaitingMessage(false);
         }
 
-        //we're setting the point in the grid orange when we're hit an enemy ship
+        // we're setting the message about turn
+        private void setTurn(String message) {
+            if(message.contains("our")) setWaitingMessage(false);
+            else setWaitingMessage(true);
+        }
+        // we're setting the point in the grid orange when we're hit an enemy ship
         private void handleHit(String message) {
         }
 
-        //we're setting the whole sunk enemy ship in red
+        // we're setting the whole sunk enemy ship in red
         private void handleHitAndSink(String message) {
         }
 
-        //we're setting the point in the grid grey when enemy miss our ship
+        // we're setting the point in the grid grey when enemy miss our ship
         private void handleMiss(String message) {
         }
 
-        //we're setting the point in the grid orange when enemy hit our ship
+        // we're setting the point in the grid orange when enemy hit our ship
         private void handleOurHit(String message) {
         }
 
-        //we're setting the whole sunk our ship in red
+        // we're setting the whole sunk our ship in red
         private void handleOurHitAndSink(String message) {
         }
 
-        //we're setting the point in the grid grey when enemy miss our ship
+        // we're setting the point in the grid grey when enemy miss our ship
         private void handleOurMiss(String message) {
         }
 
@@ -150,6 +214,30 @@ public class Controller {
                 alert.setHeaderText("There was an error during reading from the server. The game will be closed");
                 alert.showAndWait();
             });
+            try {
+                socket.close();
+                reset();
+                Thread.currentThread().interrupt();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        // popup for error in writing
+        private void writingError() {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("There was an error during writing to the server. The game will be closed");
+                alert.showAndWait();
+            });
+            try {
+                socket.close();
+                reset();
+                Thread.currentThread().interrupt();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
 
         // popup for server error in
@@ -163,7 +251,7 @@ public class Controller {
             });
         }
 
-        //poup with game information, no need to do different handlers
+        // poup with game information, no need to do different handlers
         private void gameInfo(String message) {
             Platform.runLater(() -> {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -171,6 +259,14 @@ public class Controller {
                 alert.setHeaderText(message);
                 alert.showAndWait();
                 if (message.contains("won") || message.contains("lost")) reset();
+            });
+        }
+
+        // set waiting message
+        private void setWaitingMessage(boolean set) {
+            Platform.runLater(() -> {
+                if(set)debugLabel.setText("Waiting for the server response.");
+                else debugLabel.setText("");
             });
         }
     }
