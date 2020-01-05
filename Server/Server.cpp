9dc02a4760327801f4ed36fb243c85c4c330ea2b
KamilStrozyk <strozykkamil@gmail.com>
2020-01-05 23:07:54 +0100

    Add empty Server.c and Server.h

---
+++ Server/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kamil on 05.01.2020.
+//
+
+#include "Server.h"
---
+++ Server/Server.h
@@ -0,0 +1,8 @@
+//
+// Created by kamil on 05.01.2020.
+//
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+#endif //SERVER_SERVER_H
